b7fc4d5598a853fcda7f7224ba312f701bb46dd0
shuisheng <2274751790@qq.com>
2024-06-18 19:02:36 +0800

    fix: url

--- src/api/tencentcloudCss.ts
+++ src/api/tencentcloudCss.ts
@@ -2,13 +2,10 @@ import request from '@/utils/request';
 
 export function fetchTencentcloudCssPush(liveRoomId: number) {
   return request.post<{
-    obs: {
-      url: string;
-      key: string;
-    };
-    rtmp: string;
-    flv: string;
-    hls: string;
-    webrtc: string;
+    push_rtmp_url: string;
+    push_obs_server: string;
+    push_obs_stream_key: string;
+    push_webrtc_url: string;
+    push_srt_url: string;
   }>(`/tencentcloud_css/push`, { liveRoomId });
 }
